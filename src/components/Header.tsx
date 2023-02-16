2de2334db9ba5d0aef937d2eef15ab0b037b05d3
Menna T-Allah Magdy <mennamagdykh@gmail.com>
2023-02-16 10:21:33 +0200

    add arabic bosta logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,24 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext } from "react";
 
 import Bostalogo from "../assets/bosta.png";
+import BostalogoAr from "../assets/bosta-ar.png";
+import { LangContext } from "../store/lang-Context";
 import Navbar from "./Navbar";
 import SmallScreenNavbar from "./SmallScreenNavbar";
 import styles from "./Header.module.css";
 import { useWidthHook } from "../hooks/useWidthHook";
 
 const Header = () => {
+  const ctx = useContext(LangContext);
   const width = useWidthHook();
 
   return (
     <div className={styles.header}>
-      <img src={Bostalogo} alt="logo" className={styles.logo} />
+      <img
+        src={ctx.lang === "en" ? Bostalogo : BostalogoAr}
+        alt="logo"
+        className={styles.logo}
+      />
       {width > 1200 ? <Navbar /> : <SmallScreenNavbar />}
     </div>
   );
